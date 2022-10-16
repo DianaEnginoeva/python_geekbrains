53162f9d331335fde11ef8b4ba4df1891a339384
Diana <diana@MacBook-Pro-Diana.local>
2022-10-17 01:40:53 +0300

    task2 is redone

--- homework1/task2.py
+++ homework1/task2.py
@@ -1,11 +1,14 @@
 # Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-x = int(input("Введите x: "))
-y = int(input("Введите y: "))
-z = int(input("Введите z: "))
-
 def checking(x,y,z):
 	result = not(x or y or z) == (not(x) and not(y) and not(z))
 	return result
 
-print(checking(x,y,z))
+print(checking(0,0,0))
+print(checking(0,0,1))
+print(checking(0,1,0))
+print(checking(0,1,1))
+print(checking(1,0,0))
+print(checking(1,0,1))
+print(checking(1,1,0))
+print(checking(1,1,1))
